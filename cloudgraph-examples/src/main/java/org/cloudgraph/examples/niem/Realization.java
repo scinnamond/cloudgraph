b0d9298321892757a7534ce7e7647431c2622be7
terrameta <scinnamond@gmail.com>
2013-02-02 17:54:03 -0500

    Example XSD updates

--- cloudgraph-examples/src/main/java/org/cloudgraph/examples/niem/Realization.java
+++
@@ -1,9 +0,0 @@
-package org.cloudgraph.examples.niem;
-
-import org.modeldriven.fuml.FumlObject;
-
-import fUML.Syntax.Classes.Kernel.PackageableElement;
-
-public class Realization extends PackageableElement {
-
-}
